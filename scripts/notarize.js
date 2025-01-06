b1221ceadf761ab76f53d0b40a52cc74ef89bd99
Shik <shiko.c@gmail.com>
2025-01-06 17:52:10 +0200

    Skip win/ubuntu notarize script

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
+if (process.platform !== 'darwin') {
+    console.log("Skipping notarization for non-macOS builds.");
+    process.exit(0); // Exit gracefully for other platforms
+}
+
 exports.default = async function notarizeApp(context) {
-    const { electronPlatformName, appOutDir } = context;
-    if (electronPlatformName !== 'darwin') {
-        return;
-    }
-
-    console.log('Notarizing N32B Firmware Updater...');
-
+    const { appOutDir } = context;
+    console.log("Notarizing macOS build...");
     return await notarize({
         appBundleId: 'tech.shik.updater.firmware',
         appPath: `${appOutDir}/N32B Firmware updater.app`,
